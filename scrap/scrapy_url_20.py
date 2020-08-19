c8f651bf8200a692d1554a12005b9424ff730058
Alejandro Lopez Sanchez <alejandrolopezsanchez@MacBook-Pro-de-Alejandro.local>
2020-08-20 00:00:58 +0200

    Solo guarda enlaces que pertenecen a 20 minutos y no a páginas externas

--- scrap/scrapy_url_20.py
+++ scrap/scrapy_url_20.py
@@ -3,7 +3,6 @@ from scrapy.crawler import CrawlerProcess
 import json
 from datetime import date
 
-url_list = []
 path = './data/' + str(date.today()) + '_url_20min.csv'
 
 class veinte_minutos_spider(scrapy.Spider):
@@ -11,11 +10,10 @@ class veinte_minutos_spider(scrapy.Spider):
     start_urls = ['https://www.20minutos.es/']
     
     def parse(self, response):
-
         for article in response.css('article'):
-            url = article.css('header h1 a:nth-child(1)::attr(href)').extract_first().strip()
-            url_list.append(url)
-            print(f"\"{url}\"", file=filep)
+            url = article.css('header h1 a::attr(href)').extract_first()
+            if url.startswith('https://www.20minutos.es'):
+                print(f"\"{url}\"", file=filep)
 
 process = CrawlerProcess({
     'USER_AGENT': 'Google SEO Bot'
