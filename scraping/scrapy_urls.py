9d5eab66dba89d85eff9c993f6ab949c524cc7b5
Alejandro Lopez Sanchez <alejandrolopezsanchez@Mac-mini-de-Alejandro.local>
2020-09-05 08:07:37 +0200

    saco el control de urls de la clase y lo dejo como variable global

--- scraping/scrapy_urls.py
+++ scraping/scrapy_urls.py
@@ -10,6 +10,14 @@ import numpy as np
 path = './data/urls.csv'
 today = str(date.today())
 days_number = 10
+scraped_urls = []
+
+df_urls = pd.read_csv(path)
+df_urls['date'] = pd.to_datetime(df_urls['date'], format="%Y/%m/%d")
+dates = df_urls['date']
+keep = dates > (np.datetime64(date.today() - timedelta(days=days_number)))
+df_urls = df_urls[keep]
+scraped_urls = list(df_urls['url'])
 
 
 class url_spider(scrapy.Spider):
@@ -22,33 +30,25 @@ class url_spider(scrapy.Spider):
         'https://www.lesoir.be/',
         'https://www.news.com.au/'
     ]
-    scraped_urls = []
-    df_urls = pd.DataFrame(columns=['url','date'])
 
-    def __init__(self):
-        # Si el fichero no esta vacío cargamos las urls que ya se han scrapeado y eliminamos las que tienen más de x días
-        if not os.stat(path).st_size == 0:
-            self.df_urls = pd.read_csv(path)
-            self.df_urls['date'] = pd.to_datetime(self.df_urls['date'], format="%Y/%m/%d")
-            dates = self.df_urls['date']
-            keep = dates > (np.datetime64(date.today() - timedelta(days=days_number)))
-            self.df_urls = self.df_urls[keep]
-            self.scraped_urls = list(self.df_urls['url'])
     
     def parse(self, response):
+
+        global df_urls
+        global scraped_urls
 
         urls = []
 
         if response.url.startswith('https://www.20minutos.es'):
             for article in response.css('article'):
                 url = article.css('header h1 a::attr(href)').extract_first()
-                if url.startswith('https://www.20minutos.es') and url not in self.scraped_urls:
+                if url.startswith('https://www.20minutos.es') and url not in scraped_urls:
                     urls.append(url)
                 
         elif response.url.startswith('https://www.larazon.es/'):
             urls_la_razon = set(response.css('a.story-card__areas__head::attr(href)').getall())
             for url in urls_la_razon:
-                if url not in self.scraped_urls:
+                if url not in scraped_urls:
                     urls.append(url)
 
         elif response.url.startswith('https://www.publico.es/'):
@@ -56,8 +56,16 @@ class url_spider(scrapy.Spider):
             for url in urls_publico:
                 if not url.startswith('http'):
                     url = 'https://www.publico.es' + str(url)
-                    if url not in self.scraped_urls:
+                    if url not in scraped_urls:
                         urls.append(url)
+
+        elif response.url.startswith('https://time.com'):
+            for article in response.css('h2.title'):
+                url = str(article.css('a::attr(href)').extract_first())
+                if not url.startswith('https:'):
+                    url = 'https://time.com/' + url
+                if url not in scraped_urls:
+                    urls.append(url)
                         
         elif response.url.startswith('https://www.abc.es/'):
             for article in response.css('article'):
@@ -65,7 +73,8 @@ class url_spider(scrapy.Spider):
                 if url is not None:
                     if not url.startswith('http'):
                         url = 'https://www.abc.es' + str(url)
-                    urls.append(url)
+                    if url.startswith('https://www.abc.es') and url not in scraped_urls:
+                        urls.append(url)
                   
         elif response.url.startswith('https://www.lesoir.be/'):
             for article in response.css('h4.media-heading'):
@@ -73,7 +82,8 @@ class url_spider(scrapy.Spider):
                 if url is not None:
                     if not url.startswith('http'):
                         url = 'https://www.lesoir.be' + str(url)
-                urls.append(url)
+                    if url.startswith('https://www.lesoir.be') and url not in scraped_urls:
+                            urls.append(url)
                   
         elif response.url.startswith('https://www.news.com.au/'):
             for article in response.css('h4.heading'):
@@ -81,13 +91,14 @@ class url_spider(scrapy.Spider):
                 if url is not None:
                     if not url.startswith('http'):
                         url = 'https://www.news.com.au/' + str(url)
-                    urls.append(url)
+                    if url.startswith('https://www.news.com') and url not in scraped_urls:
+                            urls.append(url)
             
 
         for url in urls:
-            self.df_urls = self.df_urls.append({'url' : url , 'date' : today} , ignore_index=True)
+            df_urls = df_urls.append({'url' : url , 'date' : today} , ignore_index=True)
         
-        self.df_urls.to_csv(path, index = False)
+        df_urls.to_csv(path, index = False)
             
 process = CrawlerProcess({
     'USER_AGENT': 'Google SEO Bot'
