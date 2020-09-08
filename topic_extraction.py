34280ffb0c95421d0e5be5a6aab3d6b9aab2a88d
Alejandro Lopez Sanchez <alejandrolopezsanchez@MacBook-Pro-de-Alejandro.local>
2020-09-08 14:35:38 +0200

    establezco un nombre fijo a la columna topic del dataframe

--- topic_extraction.py
+++ topic_extraction.py
@@ -89,7 +89,7 @@ hist_path = './data/topic_history.csv'
 for i in range(num_topics):
     words = lda_model.show_topic(i, topn = 20)
     word_dict['date'] = today
-    word_dict['Topic #' + '{:02d}'.format(i+1)] = [i[0] for i in words]
+    word_dict['Topic'] = [i[0] for i in words]
 
 topic_today = pd.DataFrame(word_dict)
 topic_today.to_csv(today_path, index=False)
