19b325c78d62693d772d5976fb96accae7659f54
Alejandro Lopez Sanchez <alejandrolopezsanchez@Mac-mini-de-Alejandro.local>
2020-09-09 22:48:32 +0200

    cambio path en remove_news

--- scraping/remove_duplicated_news.py
+++ scraping/remove_duplicated_news.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_news = pd.read_csv('../../data/news.csv')
+df_news = pd.read_csv('../data/news.csv')
 df_urls = df_news['url']
 keep = [not x for x in list(df_urls.duplicated())]
 df_news = df_news[keep]
