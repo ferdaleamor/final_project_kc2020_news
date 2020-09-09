48abbfaadb04a90df06b5e80cf188844ff3e3d6d
Alejandro Lopez Sanchez <alejandrolopezsanchez@Mac-mini-de-Alejandro.local>
2020-09-09 22:45:58 +0200

    agrego script para eliminar noticias duplicadas

---
+++ scraping/remove_duplicated_news.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df_news = pd.read_csv('../../data/news.csv')
+df_urls = df_news['url']
+keep = [not x for x in list(df_urls.duplicated())]
+df_news = df_news[keep]
+df_news.to_csv('../data/news.csv', index=False)
