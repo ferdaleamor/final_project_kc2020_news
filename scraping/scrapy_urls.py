552897a78e988120f4a8ded317c6cdc79ae05a1a
Alejandro Lopez Sanchez <alejandrolopezsanchez@MacBook-Pro-de-Alejandro.local>
2020-09-27 14:29:43 +0200

    se sobreescribe el fichero de url y no se guarda historial

--- scraping/scrapy_urls.py
+++ scraping/scrapy_urls.py
@@ -1,131 +1,90 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
-import json
+# import json
 import os
-import string
+# import string
 import pandas as pd
-from datetime import datetime, date, time, timedelta
+from datetime import date, timedelta
 import numpy as np
 
-path = '../data/urls.csv'
-today = str(date.today())
-days_number = 10
-scraped_urls = []
 
-df_urls = pd.read_csv(path)
-df_urls['date'] = pd.to_datetime(df_urls['date'], format="%Y/%m/%d")
-dates = df_urls['date']
-keep = dates > (np.datetime64(date.today() - timedelta(days=days_number)))
-df_urls = df_urls[keep]
-scraped_urls = list(df_urls['url'])
-
+def set_url(domain, url):
+    today = str(date.today())
+    if url is not None:
+        if not url.startswith('http'):
+            url = domain + str(url)
+        if url.startswith(domain):
+            print(f"\"{today}\",\"{url}\"", file=filep)
 
 class url_spider(scrapy.Spider):
+
     name = 'blogspider'
     start_urls = [
-        'https://www.20minutos.es/',
-        'https://www.larazon.es/',
-        'https://www.publico.es/',
-        'https://www.abc.es/',
-        'https://www.lesoir.be/',
-        'https://www.news.com.au/',
-        'https://time.com/',
-        'https://elpais.com/',
-        'https://www.elmundo.es/'
+        'https://www.20minutos.es',
+        'https://www.larazon.es',
+        'https://www.publico.es',
+        'https://www.abc.es',
+        'https://www.lesoir.be',
+        'https://www.news.com.au',
+        'https://time.com',
+        'https://elpais.com',
+        'https://www.elmundo.es'
     ]
 
-    
     def parse(self, response):
-
-        global df_urls
-        global scraped_urls
-
-        urls = []
 
         if response.url.startswith('https://www.20minutos.es'):
             for article in response.css('article'):
                 url = article.css('header h1 a::attr(href)').extract_first()
-                if url.startswith('https://www.20minutos.es') and url not in scraped_urls:
-                    urls.append(url)
+                set_url(response.url, url)
                 
-        elif response.url.startswith('https://www.larazon.es/'):
+        elif response.url.startswith('https://www.larazon.es'):
             urls_la_razon = set(response.css('a.story-card__areas__head::attr(href)').getall())
             for url in urls_la_razon:
-                if url not in scraped_urls:
-                    urls.append(url)
+                set_url(response.url, url)
 
-        elif response.url.startswith('https://www.publico.es/'):
+        elif response.url.startswith('https://www.publico.es'):
             urls_publico = set(response.css('a.page-link::attr(href)').getall())
             for url in urls_publico:
-                if not url.startswith('http'):
-                    url = 'https://www.publico.es' + str(url)
-                    if url not in scraped_urls:
-                        urls.append(url)
-
+                set_url(response.url, url)
+                
         elif response.url.startswith('https://www.elmundo.es'):
             urls_mundo = set(response.css('a.ue-c-cover-content__link::attr(href)').getall())
             for url in urls_mundo:
-                if url is not None:
-                    if not url.startswith('http'):
-                        url = 'https://www.elmundo.es' + str(url)
-                    if url.startswith('https://www.elmundo.es') and url not in scraped_urls:
-                        urls.append(url)
-        
+                set_url(response.url, url)
+                              
         elif response.url.startswith('https://elpais.com'):
             for article in response.css('h2.headline'):
                 url = article.css('a::attr(href)').extract_first()
-                if url is not None:
-                    if not url.startswith('http'):
-                        url = 'https://elpais.com' + str(url)
-                        if url not in scraped_urls:
-                            urls.append(url)
+                set_url(response.url, url)
 
         elif response.url.startswith('https://time.com'):
             for article in response.css('h2.title'):
                 url = str(article.css('a::attr(href)').extract_first())
-                if not url.startswith('https:'):
-                    url = 'https://time.com/' + url
-                if url.startswith('https://time.com') and url not in scraped_urls:
-                    urls.append(url)
+                set_url(response.url, url)
                         
-        elif response.url.startswith('https://www.abc.es/'):
+        elif response.url.startswith('https://www.abc.es'):
             for article in response.css('article'):
                 url = article.css('span h3 a::attr(href)').extract_first()
-                if url is not None:
-                    if not url.startswith('http'):
-                        url = 'https://www.abc.es' + str(url)
-                    if url.startswith('https://www.abc.es') and url not in scraped_urls:
-                        urls.append(url)
+                set_url(response.url, url)
                   
-        elif response.url.startswith('https://www.lesoir.be/'):
+        elif response.url.startswith('https://www.lesoir.be'):
             for article in response.css('h4.media-heading'):
                 url = article.css('a::attr(href)').extract_first()
-                if url is not None:
-                    if not url.startswith('http'):
-                        url = 'https://www.lesoir.be' + str(url)
-                    if url.startswith('https://www.lesoir.be') and url not in scraped_urls:
-                            urls.append(url)
+                set_url(response.url, url)
                   
-        elif response.url.startswith('https://www.news.com.au/'):
+        elif response.url.startswith('https://www.news.com.au'):
             for article in response.css('h4.heading'):
                 url = article.css('a::attr(href)').extract_first()
-                if url is not None:
-                    if not url.startswith('http'):
-                        url = 'https://www.news.com.au/' + str(url)
-                    if url.startswith('https://www.news.com') and url not in scraped_urls:
-                            urls.append(url)
-            
+                set_url(response.url, url)
 
-        for url in urls:
-            df_urls = df_urls.append({'url' : url , 'date' : today} , ignore_index=True)
-        
-        df_urls.to_csv(path, index = False)
-            
 process = CrawlerProcess({
     'USER_AGENT': 'Google SEO Bot'
 })
 
+path = '../data/urls.csv'
 filep = open(path, 'w')
+print(f"date,url", file=filep)
 process.crawl(url_spider)
 process.start()
 filep.close()
